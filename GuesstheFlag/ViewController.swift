f21dd86999f67d102d90f0d51cc495e732bc13fb
Miraç Doğan <miracdogn@gmail.com>
2022-11-01 23:45:10 +0300

    Flags added to the project and assigned to buttons

--- GuesstheFlag/ViewController.swift
+++ GuesstheFlag/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        countries += ["estonia", "ireland", "italy", "poland", "russia",
+        "france", "germany", "monaco", "nigeria", "spain", "uk", "us"]
+    
+        asQuestion()
+        [button1, button2, button3].forEach {
+                    $0?.imageView?.layer.borderWidth = 1
+                    $0?.imageView?.layer.borderColor = UIColor.lightGray.cgColor
+                }
     }
-
+    
+    func asQuestion() {
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+    }
 
 }
 

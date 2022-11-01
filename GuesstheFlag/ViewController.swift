713007dade0d07c38afff4d4a1c6aaaf8d618a47
Miraç Doğan <miracdogn@gmail.com>
2022-11-01 23:17:45 +0300

    Initial Commit

---
+++ GuesstheFlag/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GuesstheFlag
+//
+//  Created by Miraç Doğan on 1.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
